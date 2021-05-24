a1884fd4961a0905245e07fb2b32725be3a65541
Zhao Hongjue <zhaohongjue123@163.com>
2021-05-24 09:36:57 +0800

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,24 @@
 #include "Room.h"
 using namespace std;
 
+void test();
+void prompt();
+
 int main()
+{
+    test();
+    return 0;
+}
+
+void test()
 {
     Role Hero("Hero");
     Room test;
     cout << Hero << endl;
     cout << test << endl;
-    return 0;
+}
+
+void prompt()
+{
+    cout << "Enter your command:" << endl;
 }
